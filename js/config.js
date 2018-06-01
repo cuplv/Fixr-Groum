28702fb439c74a83620a0174edd749c7139d7974
smover <sergio.mover@gmail.com>
2018-05-31 20:00:22 -0600

    Fix port

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 let config = {
   // compute_url: "http://13.58.122.176:8081/compute/method/groums",
   // provenance : "http://13.58.122.176:8081/query/provenance/groums?"
-  compute_url: "http://localhost:30072/compute/method/groums",
-  provenance : "http://localhost:30072/query/provenance/groums?"
+  compute_url: "http://localhost:8081/compute/method/groums",
+  provenance : "http://localhost:8081/query/provenance/groums?"
 }
 
 global.config = config
