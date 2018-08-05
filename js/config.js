85d798caf24b7e157389ce35c3ae902da566fbc0
smover <sergio.mover@gmail.com>
2018-08-05 14:46:32 -0600

    Add separate configs for devel and production

--- js/config.js
+++ js/config.js
@@ -1,11 +1,23 @@
-let config = {
+const env = process.env.NODE_ENV;
+
+let develop = {
   compute_url: "http://localhost:30072/compute/method/groums",
   provenance : "http://localhost:30072/query/provenance/groums",
-  // compute_url: "http://localhost:8081/compute/method/groums",
-  // provenance : "http://localhost:8081/query/provenance/groums?",
+  srcServiceUrl : "http://localhost:8088/src"
+}
+
+let production = {
+  compute_url: "http://localhost:30072/compute/method/groums",
+  provenance : "http://localhost:30072/query/provenance/groums",
   srcServiceUrl : "http://localhost:8080/src"
 }
 
-global.config = config
 
+
+let config_array = {"develop" : develop,
+                    "production" : production}
+
+let config = config_array[env]
+
+global.config = config;
 export default config
