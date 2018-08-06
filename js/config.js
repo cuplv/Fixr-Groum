111b243185e397652ca8ca84e0336fb34f63b7e7
smover <sergio.mover@gmail.com>
2018-08-06 08:47:35 -0600

    Fix biggroum service url for production

--- js/config.js
+++ js/config.js
@@ -7,8 +7,8 @@ let develop = {
 }
 
 let production = {
-  compute_url: "http://biggroum:30072/compute/method/groums",
-  provenance : "http://biggroum:30072/query/provenance/groums",
+  compute_url: "http://biggroum_search:30072/compute/method/groums",
+  provenance : "http://biggroum_search:30072/query/provenance/groums",
   srcServiceUrl : "http://srcfinder:8080/src"
 }
 
